d9d9eec777a2f3f8323fd6e6b3f2d48de438a1a8
kcsikarwar <kcsikarwar@gmail.com>
2017-03-28 22:46:45 +0530

    user class changes

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
+export class User {
+  firstname: string;
+  lastname: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-user-detail',
@@ -8,14 +13,14 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
-
   id: number;
+  user: User[];
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = params['id'];
-
+      this.user = new User("ksdfsd",  'Mr. Nice', 'email');
     });
   }
 
